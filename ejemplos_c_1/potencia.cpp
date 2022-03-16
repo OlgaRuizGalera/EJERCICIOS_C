5f6d3d09464a072945c91bb1f96b0db852cc0ce6
OlgaRuizGalera <olgaruizg@correo.ugr.es>
2022-03-16 18:32:29 +0100

    a

---
+++ ejemplos_c_1/potencia.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (void)
+{
+    int n, m, a;
+    cout <<"Numero cualquiera: ";
+    cin >> n;
+    m=n*n;
+    cout << m;
+    cin >> a;
+    return 0;
+    
+}
